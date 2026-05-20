970c9107e7201012f569f9412f3c7b329f54226e
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2026-05-20 11:48:52 +0500

    20-05-2026

---
+++ 3rd-Semester/CS-304/Mid-Term-Preparation/oop.cpp
@@ -0,0 +1,107 @@
+// Lectrue 01:-
+
+// What is object orintation?
+
+// It is a technique in which we visualize our programming problems in the form of objects and their interactions as happens in real life.
+
+// Example:
+
+// Suppose we want to develop a fee collection system for a school for this we will need to find out related objects and their interactions as happens in real life.
+
+// What is object?
+
+// Object can be tangible or intangible which have certain attributes and behaviors.
+
+// What is model?
+
+// Model is an abstraction of something real or conceptual.
+
+// Example:
+
+// Highway Maps
+// Mechanical Models
+
+// Object Oriented Models:
+
+// An object-oriented model (OOM) is a conceptual framework that visualizes, specifies, constructs, and documents a system by structuring it around real-world "objects" that combine both data and behavior.
+
+// Advantage OOP:
+
+// We can easily develop, understand and implement it using OOPLs.
+
+// Summary:
+
+/*
+• Model is the abstraction of some real word scenario. It helps us to understand
+that scenario.
+• Object oriented model of any scenario (problem) describes that scenario
+(problem) in the form of interacting objects.
+• We use Object Orientation because it helps us in mapping real world problem
+in a programming language.
+• Object Orientation is achieved using objects and their relationships.
+• Properties of an object are described using its data members and behavior of an
+object is described using its functions.
+• Objects may be tangible (physical) or intangible (also called conceptual or
+virtual).
+• Generally when we have given a certain problem description, nouns in that
+problem description are candidates for becoming objects of our system.
+• There may be more than one aspects of an object
+• It is not necessary that every object has a specific role in implementation of a
+problem there may be some objects without any role, like school parking in
+our school.
+• It is easier to develop programs using Object Oriented Programming because
+it is closer to real life.
+
+*/
+
+// Lectrue 02:-
+
+// Information hiding:
+
+// Information hiding is one of the concept inspired from real life which says that all information should be not accessible to the all persons. It means that showing only necessary details to the outside world and all other details should remain private.
+
+// Advantages of information hiding:
+
+// It simplifies our object oriented mode.
+// It is a barrier against change propogation.
+
+// Information hiding is implemented using the Encapsulation and Abstraction.
+
+// Encapsulation:
+
+// Encapsulation means that encapsulating the attributes and behaviors in one unit.
+
+// Advantages of Encapsulation:
+
+// Better clarity and simplicity
+// Less complexity
+// Better understanding
+
+// Interface:
+
+// Interface is a set of functions of an object that he want to expose to other objects. It is necessary for a function to execute the communication.
+
+// How to implement interface in the object?
+
+// We need data stuructres to hold data and functions to set required behavior.
+
+// like take the example of contact details saved in the SIM of a phone, In that case we can say physical structure of SIM card as Data Structure And Read/write operations provided by the phone as Functionality.
+
+// Message:
+
+// Objects communicate through messages they send messages (stimuli) by invoking appropriate operations on the target object. The number and kind of messages that can be sent to an object depends upon its interface.
+
+// A Person sends message (stimulus) “stop” to a Car by applying brakes.
+// A Person sends message “place call” to a Phone by pressing appropriate button.
+
+// 02.7. Summary
+
+/*
+• Information hiding is achieved through encapsulation.
+• Encapsulation and Information Hiding are related to each other.
+• Interface of an object provides us the list of available functions.
+• An object may have more than one interface.
+• Interface and implementation are separated from each other to achieve
+Information Hiding.
+• Objects communicate with each other using messages.
+*/
